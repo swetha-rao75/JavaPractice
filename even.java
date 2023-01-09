942252cd0e620139c9dfac56dfc1738f244acfca
swetha-rao75 <121069466+swetha-rao75@users.noreply.github.com>
2023-01-09 11:36:22 -0600

    Update even.java

--- even.java
+++ even.java
@@ -4,7 +4,7 @@ public class even{
      * @param args
      */
     public static void main(String[]args){
-        for(int x=1; x<100; x++){
+        for(int x=1; x<=100; x++){
             if(x%2==0 && x%5==0){
             System.out.println(x);
             }
